a0d1a55aed09961b3414d0c6f12cbc14b6547a8b
Craig Ching <craigching@gmail.com>
2018-04-25 23:45:22 -0500

    Make some services Guice Singletons

--- src/main/java/net/webasap/nextbus/modules/BusServiceModule.java
+++ src/main/java/net/webasap/nextbus/modules/BusServiceModule.java
@@ -1,6 +1,7 @@
 package net.webasap.nextbus.modules;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 import net.webasap.nextbus.services.TimeToNextBusService;
 import net.webasap.nextbus.services.HttpClient;
 import net.webasap.nextbus.services.MetroTransitService;
@@ -10,8 +11,13 @@ import net.webasap.nextbus.services.impl.OkHttpClientImpl;
 public class BusServiceModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(TimeToNextBusService.class);
-        bind(HttpClient.class).to(OkHttpClientImpl.class);
-        bind(MetroTransitService.class).to(MetroTransitServiceImpl.class);
+        bind(TimeToNextBusService.class)
+                .in(Singleton.class);
+        bind(HttpClient.class)
+                .to(OkHttpClientImpl.class)
+                .in(Singleton.class);
+        bind(MetroTransitService.class)
+                .to(MetroTransitServiceImpl.class)
+                .in(Singleton.class);
     }
 }
