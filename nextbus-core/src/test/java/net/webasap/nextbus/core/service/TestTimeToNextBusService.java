3af9ca3b557554757e13ea91ee148fb991b4f890
Craig Ching <craigching@gmail.com>
2018-04-27 12:43:41 -0500

    Adding an initial test suite for TimeToNextBusService

---
+++ nextbus-core/src/test/java/net/webasap/nextbus/core/service/TestTimeToNextBusService.java
@@ -0,0 +1,159 @@
+package net.webasap.nextbus.core.service;
+
+import lombok.val;
+import net.webasap.nextbus.core.BaseJsonTestSuite;
+import net.webasap.nextbus.core.services.HttpClient;
+import net.webasap.nextbus.core.services.TimeToNextBusService;
+import net.webasap.nextbus.core.services.impl.MetroTransitServiceImpl;
+import net.webasap.nextbus.core.utilities.RefTime;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.time.ZonedDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestTimeToNextBusService extends BaseJsonTestSuite {
+
+    @Test
+    public void testHappyPath() {
+
+        // The departure we are targeting is at 9:45 and we want the output to
+        // be "5 minutes", so specify our ref time as 9:40.  See departures.json
+        val refTimeStr = "2018-04-25T09:40:00-05:00";
+        val refTime = ZonedDateTime.parse(refTimeStr);
+
+        val routeText = "METRO Blue Line";
+        val directionText = "south";
+        val stopText = "Target Field Station Platform 1";
+
+        val routeIdText = "901"; // METRO Blue Line
+        val directionIdText = "1"; // SOUTHBOUND
+        val stopIdText = "TF12"; // Target Field Station Platform 1
+
+        val routesUrl = "http://svc.metrotransit.org/NexTrip/Routes";
+        val directionsUrl = String.format("http://svc.metrotransit.org/NexTrip/Directions/%s", routeIdText);
+        val stopsUrl = String.format("http://svc.metrotransit.org/NexTrip/Stops/%s/%s", routeIdText, directionIdText);
+        val departuresUrl = String.format("http://svc.metrotransit.org/NexTrip/%s/%s/%s", routeIdText, directionIdText, stopIdText);
+
+        val mockHttpClient = mock(HttpClient.class);
+        when(mockHttpClient.get(routesUrl)).thenReturn(ROUTES_JSON);
+        when(mockHttpClient.get(directionsUrl)).thenReturn(DIRECTIONS_JSON);
+        when(mockHttpClient.get(stopsUrl)).thenReturn(METRO_BLUE_LINE_STOPS_JSON);
+        when(mockHttpClient.get(departuresUrl)).thenReturn(DEPARTURES_JSON);
+
+        val mockRefTime = mock(RefTime.class);
+        when(mockRefTime.getRefTime()).thenReturn(refTime);
+
+        val busService = new TimeToNextBusService(
+                new MetroTransitServiceImpl(mockHttpClient),
+                mockRefTime);
+
+        val message = busService.getTimeToNextBus(routeText, stopText, directionText);
+
+        assertEquals("5 minutes", message);
+    }
+
+    @Test
+    public void testUnknownRoute() {
+
+        val routesUrl = "http://svc.metrotransit.org/NexTrip/Routes";
+
+        val mockHttpClient = mock(HttpClient.class);
+        when(mockHttpClient.get(routesUrl)).thenReturn(ROUTES_JSON);
+
+        val busService = new TimeToNextBusService(
+                new MetroTransitServiceImpl(mockHttpClient),
+                new RefTime());
+
+        val message = busService.getTimeToNextBus("blah", "", "");
+
+        assertEquals("No matches were found for the given route.", message);
+    }
+
+    @Test
+    public void testMulitpleMatchingRoutes() {
+
+
+        val routesUrl = "http://svc.metrotransit.org/NexTrip/Routes";
+
+        val mockHttpClient = mock(HttpClient.class);
+        when(mockHttpClient.get(routesUrl)).thenReturn(ROUTES_JSON);
+
+        val busService = new TimeToNextBusService(
+                new MetroTransitServiceImpl(mockHttpClient),
+                new RefTime());
+
+        val message = busService.getTimeToNextBus("METRO", "", "east");
+
+        assertEquals("More than one match was found for the given route.", message);
+    }
+
+    @Test
+    public void testBadDirection() {
+        val routeIdText = "901"; // METRO Blue Line
+
+        val routesUrl = "http://svc.metrotransit.org/NexTrip/Routes";
+        val directionsUrl = String.format("http://svc.metrotransit.org/NexTrip/Directions/%s", routeIdText);
+
+        val mockHttpClient = mock(HttpClient.class);
+        when(mockHttpClient.get(routesUrl)).thenReturn(ROUTES_JSON);
+        when(mockHttpClient.get(directionsUrl)).thenReturn(DIRECTIONS_JSON);
+
+        val busService = new TimeToNextBusService(
+                new MetroTransitServiceImpl(mockHttpClient),
+                new RefTime());
+
+        val message = busService.getTimeToNextBus("METRO Blue Line", "", "east");
+
+        assertEquals("The provided direction is not valid for the given route.", message);
+    }
+
+    @Test
+    public void testUnknownStop() {
+        val routeIdText = "901"; // METRO Blue Line
+        val directionIdText = "1"; // SOUTHBOUND
+
+        val routesUrl = "http://svc.metrotransit.org/NexTrip/Routes";
+        val directionsUrl = String.format("http://svc.metrotransit.org/NexTrip/Directions/%s", routeIdText);
+        val stopsUrl = String.format("http://svc.metrotransit.org/NexTrip/Stops/%s/%s", routeIdText, directionIdText);
+
+        val mockHttpClient = mock(HttpClient.class);
+        when(mockHttpClient.get(routesUrl)).thenReturn(ROUTES_JSON);
+        when(mockHttpClient.get(directionsUrl)).thenReturn(DIRECTIONS_JSON);
+        when(mockHttpClient.get(stopsUrl)).thenReturn(METRO_BLUE_LINE_STOPS_JSON);
+
+        val busService = new TimeToNextBusService(
+                new MetroTransitServiceImpl(mockHttpClient),
+                new RefTime());
+
+        val message = busService.getTimeToNextBus("METRO Blue Line", "blah", "south");
+
+        assertEquals("No stop was found for the given stop, route, and direction.", message);
+    }
+
+    @Test
+    public void testMultipleStops() {
+        val routeIdText = "901"; // METRO Blue Line
+        val directionIdText = "1"; // SOUTHBOUND
+
+        val routesUrl = "http://svc.metrotransit.org/NexTrip/Routes";
+        val directionsUrl = String.format("http://svc.metrotransit.org/NexTrip/Directions/%s", routeIdText);
+        val stopsUrl = String.format("http://svc.metrotransit.org/NexTrip/Stops/%s/%s", routeIdText, directionIdText);
+
+        val mockHttpClient = mock(HttpClient.class);
+        when(mockHttpClient.get(routesUrl)).thenReturn(ROUTES_JSON);
+        when(mockHttpClient.get(directionsUrl)).thenReturn(DIRECTIONS_JSON);
+        when(mockHttpClient.get(stopsUrl)).thenReturn(METRO_BLUE_LINE_STOPS_JSON);
+
+        val busService = new TimeToNextBusService(
+                new MetroTransitServiceImpl(mockHttpClient),
+                new RefTime());
+
+        val message = busService.getTimeToNextBus("METRO Blue Line", "Target Field Station", "south");
+
+        assertEquals("More than one stop was found for the given stop, route, and direction.", message);
+    }
+}
